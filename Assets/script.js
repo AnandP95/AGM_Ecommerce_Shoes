8a4b10087f13900b2ebbf47d6ba3babd88599678
Malka G <=>
2023-08-09 21:10:05 -0400

    updated script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,18 @@
 
+var dropdownOptions = [];
 var currencyRates = function(){
-    var inputRequestUrl = "https://api.currencylayer.com/live?access_key=8ee7c486fccf59cf7db683a14a0f03c4";
+    var inputRequestUrl = "http://api.currencylayer.com/live?access_key=8ee7c486fccf59cf7db683a14a0f03c4";
     fetch(inputRequestUrl)
     .then(function (response) {
         console.log("currency Api \n"+ response.status);
         return response.json();
-    })
-    // .then(function (data) {
-    //   latitude = data.coord.lat;
-    //   longitude = data.coord.lon;
-    //   console.log(latitude);
-    //   console.log(longitude);
-    //   getData(longitude,latitude);
-    //   $(".results").css("display","block");
-    // });
+    }).then(function (data) {
+        // put these values into a dropdown list for the user to choose a currency to change to. 
+        var quotes = Object.keys(data.quotes);
+        for(var i = 0; i < quotes.length; i++){
+            console.log(quotes[i]);
+        }
+    });
 }
 currencyRates();
 
@@ -24,7 +23,7 @@ var getProductsInfo = function() {
         console.log("products Api \n"+response.status);
         return response.json();
     }).then(function (data) {
-          for(var i = 0; i < 10; i++){
+          for(var i = 0; i < 12; i++){
             var product  = `<li class="product">
             <img src="${data[i].image}" alt=""> 
             <h2>${data[i].title}</h2>
@@ -32,7 +31,7 @@ var getProductsInfo = function() {
             <span class="price">$${data[i].price}</span>
             <button>Add to Cart</button>
             </li> `;
-            console.log(product);
+            // console.log(product);
             $("#productList").append(product);
             $(".productDescription").css("display","none");
           }
